d4824ff7fb3a057a027a31d9c4a3e6e1f06976ef
ericechemane <eechemane29@gmail.com>
2022-03-03 06:54:11 +0800

    radio and checkbox updated

--- src/routes/radio.svelte
+++ src/routes/radio.svelte
@@ -1,14 +1,14 @@
 <main style="padding: 2rem;">
 	<label for="light" class="radio-container">
-		Light
 		<input type="radio" name="theme" id="light" />
 		<span class="radio" />
+		Light
 	</label>
 	<br />
 	<br />
 	<label for="dark" class="radio-container">
-		Light
 		<input type="radio" name="theme" id="dark" />
 		<span class="radio" />
+		Light
 	</label>
 </main>
