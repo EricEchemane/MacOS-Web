10275a3a4515838bf88ba73e8668711dbf0604bd
ericechemane <eechemane29@gmail.com>
2022-03-03 06:00:47 +0800

    switch updated

--- src/routes/switch.svelte
+++ src/routes/switch.svelte
@@ -1,6 +1,6 @@
 <br />
 <br />
 <input type="checkbox" role="switch" />
+<input type="checkbox" role="switch" checked class="small" />
 <input type="checkbox" role="switch" disabled />
-<input type="checkbox" role="switch" checked />
 <input type="checkbox" role="switch" disabled checked />
