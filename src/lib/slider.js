e7ff1b742302e4e84d9e7880fd7d70346f8c6be1
ericechemane <eechemane29@gmail.com>
2022-03-03 07:14:03 +0800

    slider added

---
+++ src/lib/slider.js
@@ -0,0 +1,18 @@
+export const fillSliderTracks = () => {
+    const sliders = document.querySelectorAll('input[type="range"]');
+
+    const rangeToPercent = (slider) => {
+        const max = slider.getAttribute("max");
+        const percent = (slider.value / max) * 100;
+
+        return `${parseInt(percent)}%`;
+    };
+
+    sliders.forEach((slider) => {
+        slider.style.setProperty("--track-fill", rangeToPercent(slider));
+
+        slider.addEventListener("input", (e) => {
+            e.target.style.setProperty("--track-fill", rangeToPercent(e.target));
+        });
+    });
+}
---
+++ src/routes/slider.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { onMount } from 'svelte';
+	import { fillSliderTracks } from '$lib/slider';
+
+	onMount(() => {
+		fillSliderTracks();
+	});
+</script>
+
+<br />
+<br />
+<input type="range" max="10" step=".1" />
