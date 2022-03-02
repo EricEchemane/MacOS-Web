5930717f5d7ad52b40d70c2cba27c9ac682d4b90
ericechemane <eechemane29@gmail.com>
2022-03-03 06:21:22 +0800

    select added

---
+++ src/routes/select.svelte
@@ -0,0 +1,16 @@
+<br />
+<br />
+<select name="colors" id="colors">
+	<option hidden>Colors</option>
+	<option value="1">Red</option>
+	<option value="2">Blue</option>
+	<option value="3">Green</option>
+</select>
+<br />
+<br />
+<select name="colors" id="colors" disabled>
+	<option hidden>Colors</option>
+	<option value="1">Red</option>
+	<option value="2">Blue</option>
+	<option value="3">Green</option>
+</select>
