7ac079605b94ca0865eb0c8be4071fbb017bb938
ericechemane <eechemane29@gmail.com>
2022-03-03 10:57:04 +0800

    btn group added

---
+++ src/routes/button-group.svelte
@@ -0,0 +1,22 @@
+<br />
+<br />
+<div class="button-group-wrapper">
+	<label for="small" class="button-group-option">
+		<input type="checkbox" name="size" id="small" checked value="left" />
+		<span class="button">
+			<i class="bx bx-align-left" />
+		</span>
+	</label>
+	<label for="Medium" class="button-group-option">
+		<input type="checkbox" name="size" id="Medium" value="center" />
+		<span class="button">
+			<i class="bx bx-align-middle" />
+		</span>
+	</label>
+	<label for="Large" class="button-group-option">
+		<input type="checkbox" name="size" id="Large" value="right" />
+		<span class="button">
+			<i class="bx bx-align-right" />
+		</span>
+	</label>
+</div>
