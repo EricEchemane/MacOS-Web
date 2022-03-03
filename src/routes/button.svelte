02157843d0fa7afc7b178d729825d06e405b3a1e
ericechemane <eechemane29@gmail.com>
2022-03-03 09:26:02 +0800

    added icon button

--- src/routes/button.svelte
+++ src/routes/button.svelte
@@ -5,4 +5,18 @@
 	<button disabled>Disabled</button>
 	<button class="filled"> continue </button>
 	<button class="filled" disabled> continue </button>
+	<br />
+	<br />
+	<div class="icon-button" role="button">
+		<i class="bx bx-sun" />
+	</div>
+	<div class="icon-button" role="button">
+		<i class="bx bx-moon" />
+	</div>
+	<div class="icon-button disabled" role="button">
+		<i class="bx bx-skip-previous" />
+	</div>
+	<div class="icon-button" role="button">
+		<i class="bx bx-skip-next" />
+	</div>
 </main>
