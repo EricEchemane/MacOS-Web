050eda52c84cc1567ce6e39737654ae559009d8e
ericechemane <eechemane29@gmail.com>
2022-03-03 11:53:42 +0800

    Added dropdown

---
+++ src/routes/dropdown.svelte
@@ -0,0 +1,19 @@
+<br />
+<br />
+<div class="menu-wrapper">
+	<div class="menu-label" role="button">File</div>
+	<div class="menu-items" role="option">
+		<div class="menu-item">About</div>
+		<div class="menu-item">Preferences</div>
+		<div class="menu-item">Save</div>
+		<div class="menu-item">Exit</div>
+	</div>
+</div>
+<div class="menu-wrapper">
+	<div class="menu-label" role="button">Edit</div>
+	<div class="menu-items" role="option">
+		<div class="menu-item">Undo</div>
+		<div class="menu-item">Redo</div>
+		<div class="menu-item">Cut</div>
+	</div>
+</div>
