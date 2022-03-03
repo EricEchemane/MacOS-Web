1738dfa8683cc38037ce849ca0f6061dc7fe9135
ericechemane <eechemane29@gmail.com>
2022-03-03 08:12:36 +0800

    text, search, password input added

---
+++ src/routes/textinput.svelte
@@ -0,0 +1,9 @@
+<br />
+<br />
+<input type="text" placeholder="Placeholder" />
+<br />
+<br />
+<input type="password" placeholder="Password" disabled />
+<br />
+<br />
+<input type="search" placeholder="Search" />
