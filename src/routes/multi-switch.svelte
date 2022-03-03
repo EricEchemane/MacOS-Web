31cb60c6c7ca23c759c1d78f3810771a70686303
ericechemane <eechemane29@gmail.com>
2022-03-03 08:51:01 +0800

    Added multi-switch

---
+++ src/routes/multi-switch.svelte
@@ -0,0 +1,16 @@
+<br />
+<br />
+<div class="multi-switch-option-wrapper">
+	<label for="small" class="multi-switch-option">
+		<input type="radio" name="size" id="small" checked />
+		<span class="text"> Small </span>
+	</label>
+	<label for="Medium" class="multi-switch-option">
+		<input type="radio" name="size" id="Medium" />
+		<span class="text"> Medium </span>
+	</label>
+	<label for="Large" class="multi-switch-option">
+		<input type="radio" name="size" id="Large" />
+		<span class="text"> Large </span>
+	</label>
+</div>
